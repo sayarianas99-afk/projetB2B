457e678900cbc79a9efd5875425d1f13b899a4ad
sayarianas99-afk <sayarianas99@gmail.com>
2026-06-10 09:41:29 -1100

    Initial frontend commit

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const categorySchema = new mongoose.Schema({
   name:        { type: String, required: true, trim: true },
   slug:        { type: String, required: true, unique: true, lowercase: true, trim: true },
