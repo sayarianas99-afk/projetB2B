f92ddd5f494ffd10d81efdd6dbe856504b65ea07
sayarianas99-afk <sayarianas99@gmail.com>
2026-06-11 04:24:05 -1100

    fix url vercel corse

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,17 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL || '/api',
-// });
-
-// // Interceptor to attach the JWT token from localStorage to all outgoing requests
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://projetb2b-production.up.railway.app/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
